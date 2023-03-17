974dc138aff50c7595f55d91e3ed3c9973ec2d03
Joseph Miseda <misedajoseph@gmail.com>
2023-03-17 16:30:43 +0300

    64 vs 32

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long int d;
 	long long int e;
 
-	printf("Size of a char: %ld bytes(s)\n", sizeof(b));
-	printf("Size of a int: %ld bytes(s)\n", sizeof(a));
-	printf("Size of a long int: %ld bytes(s)\n", sizeof(d));
-	printf("Size of a long long int: %ld bytes(s)\n", sizeof(e));
-	printf("Size of a float: %ld bytes(s)\n", sizeof(c));
+	printf("Size of a char: %ld byte(s)\n", sizeof(b));
+	printf("Size of an int: %ld byte(s)\n", sizeof(a));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(d));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(e));
+	printf("Size of a float: %ld byte(s)\n", sizeof(c));
 	return (0);
 }
