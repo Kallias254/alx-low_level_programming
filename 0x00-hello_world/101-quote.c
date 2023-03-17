ff58515b50ed10c4a184e64efbe84dac25f7f778
Joseph Miseda <misedajoseph@gmail.com>
2023-03-17 17:49:49 +0300

    UNIX Last fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 
 /**
  * main - print to stderr
- * Description - and that piece of art is useful\" - Dor a Korpar, 2015-10-19
+ * Description - and that piece of art is useful\" - Dora Korpar, 2015-10-19
  * Return: return 1
  */
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dor a Korpar, 2015-10-19";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	fprintf(stderr, "%s\n", msg);
 
