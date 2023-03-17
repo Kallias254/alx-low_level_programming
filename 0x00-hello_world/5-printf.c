fd32235398ef841b518e15262bcf94260643d2ad
MJ254 <misedajoseph@gmail.com>
2023-03-16 19:44:29 -0700

    Betty Yeeees

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -11,8 +11,8 @@ int main(void)
 	 * printf_action - Prints a string using printf
 	 */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	 /**
-         * Return: return 0 
-         */
+	/**
+	* Return:return 0
+	*/
 	return (0);
 }
