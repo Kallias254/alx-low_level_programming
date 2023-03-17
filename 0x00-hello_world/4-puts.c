937f112eadc994ce4ac75dd716a0eec32f13f35b
MJ254 <misedajoseph@gmail.com>
2023-03-16 18:30:41 -0700

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * Printing the best sentence in the world
+ * Desription:Inafanya ile kitu
+ *
+ * Return: zero
+ */
 int main(void)
 {
 	char best_sentence[] = "\"Programming is like building a multilingual puzzle";
