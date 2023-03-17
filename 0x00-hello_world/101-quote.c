34e6a31060fe6a9cb088511837ac6aed3bf9fb94
Joseph Miseda <misedajoseph@gmail.com>
2023-03-17 17:45:45 +0300

    Simple unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,14 @@
 
 /**
  * main - print to stderr
- * description - and that piece of art is useful\" - Dora Korpar, 2015-10-19
- * return - return 1
+ * Description - and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ * Return: return 1
  */
 int main(void)
 {
-	fprintf(stderr, "%s\n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	char msg[] = "and that piece of art is useful\" - Dor a Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", msg);
 
 	return (1);
 }
