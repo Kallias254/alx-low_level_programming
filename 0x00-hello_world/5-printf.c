14736a4c819ea7761dfaef44c5c21894aad8e4d7
MJ254 <misedajoseph@gmail.com>
2023-03-16 19:24:15 -0700

    Assember x2

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main- print exactly as is
+ *
+ * Return: 0
+ */
+int main(void)
+{
+/**
+ * printf_action - Prints a string using printf
+ */
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
