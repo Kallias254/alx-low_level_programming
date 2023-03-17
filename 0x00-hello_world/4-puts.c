d977f3cbe785a88c2a4a48df9d901300f5f6dbcc
MJ254 <misedajoseph@gmail.com>
2023-03-16 19:02:03 -0700

    Betty anatesa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 
 /**
- * function_name - Short description, single line
- * @parameterx: Description of parameter best_sentence
-(* a blank line
- * Description: Longer description of the function)?
-(* section header: Section description)*
+ * main- prints sentence
  * Return: Description of the returned value
  */
 int main(void)
 {
 	/**
-	 * Inafanya ile kitu
-	 */
-	char best_sentence[] = "\"Programming is like building a multilingual puzzle";
-
-	/**
 	 * printing
 	 */
-	puts(best_sentence);
+	puts("\"Programming is like building a multilingual puzzle");
 	/**
 	 * inareturn ile kitu
 	 */
