4e21f6dafaf6a81371ae706c18099281d91e71e7
Joseph Miseda <misedajoseph@gmail.com>
2023-03-17 17:32:47 +0300

    Simple linux

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print to stderr
+ * description - and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ * return - return 1
+ */
+int main(void)
+{
+	fprintf(stderr, "%s\n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
