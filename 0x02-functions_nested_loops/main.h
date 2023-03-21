5b8aff64a49d3f00c79bd1781c1b79e90cc913d7
Joseph Miseda <misedajoseph@gmail.com>
2023-03-21 23:24:01 +0300

    The alphabet game - directory fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <unistd.h>
 
-/**
- *Declarations for custom function _putchar
- *Description: main file for _putchar custom function
- *Return: 1 success always
- */
 
+int _putchar(char c);
 void print_alphabet(void);
 #endif
