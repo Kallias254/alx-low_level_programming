cb6f4f14593bdb131fa7f57223856abd18fec2fc
Joseph Miseda <misedajoseph@gmail.com>
2023-03-23 16:30:32 +0300

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *add - function that add two integers
+ *@int: 1 of the numbers
+ *@int: 2 of the numbers
+ *Return: Returns the result.
+ *
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *times_table - print the 9 times table
+ *Return: 10 lines of 9 multiple table
+ */
+int times_table(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+	int g;
+	int h;
+	int i;
+	int j;
+
+for (i = 0 ; i <= 9 ; i++)
+    {
+	    a = 0;
+	    while (i <= 0)
+	    {
+		    _putchar('0' + a);
+		    a++;
+	    }
+
+	    while (b <= i)
+	    {
+		    _putchar('0' + b);
+	    }
+    }
+}
