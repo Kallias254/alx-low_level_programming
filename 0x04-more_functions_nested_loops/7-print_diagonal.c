188521d5787167c4df11c94996d40df0fbbe6035
Joseph Miseda <misedajoseph@gmail.com>
2023-03-25 16:15:38 +0300

    Diagonally parked

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,17 +8,26 @@
 void print_diagonal(int n)
 {
 	int i;
+	int b;
 	char stroke;
+	int spaces = 0;
 
 	stroke = '\\';
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	for (i = 0 ; i <= n ; i++)
+	} else
 	{
-		_putchar(' ');
-	}
+	for (i = 0 ; i < n ; i++)
+	{
+		for (b = 0 ; b <= spaces ; b++)
+		{
+			if (b != 0)
+				_putchar(' ');
+		}
 		_putchar(stroke);
 		_putchar('\n');
+		spaces++;
+	}
+	}
 }
