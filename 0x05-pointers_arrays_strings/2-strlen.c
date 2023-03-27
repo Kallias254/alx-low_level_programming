19787da33b470d57e19e5d62d3d8d0bc79d0b4ff
Joseph Miseda <misedajoseph@gmail.com>
2023-03-28 00:21:51 +0300

    I do not fear computers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string.
+ *@s: the string array passed
+ * Return: Always 0 successful.
+ *
+ */
+int _strlen(char *s)
+{
+	int result = sizeof(s) / sizeof(char);
+
+return (result);
+}
