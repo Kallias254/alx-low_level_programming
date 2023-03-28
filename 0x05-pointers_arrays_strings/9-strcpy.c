80f242b293a8d9ded31f315a12f59c3ace3e58eb
Joseph Miseda <misedajoseph@gmail.com>
2023-03-29 01:13:37 +0300

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies a string from src to dest
+ *@dest: The destination
+ *@src: The source
+ *Return: the pointer to the destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+	{
+		dest[i] = src[i];
+	}
+		dest[i] = src[i];
+return (dest);
+}
