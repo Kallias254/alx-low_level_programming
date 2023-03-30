016a64eb5b4e515f34c6157b33991f1a00a5d3af
Joseph Miseda <misedajoseph@gmail.com>
2023-03-30 10:14:55 +0300

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ **_strcat - custom function to concatenate two strings
+ *@dest: destination
+ *@src: the source string
+ *Return: the string of the destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	for (a = 0 ; dest[a] != '\0' ; a++);
+	
+	for (b = 0 ; src[b] != '\0' ; b++)
+		dest[a + b] = src[b];
+	dest[a + b] = '\0';
+return (dest);
+}
