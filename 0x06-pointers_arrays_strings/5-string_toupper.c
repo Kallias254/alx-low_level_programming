f7e3af062dabdc1cbd904ef7fdf86508d3a4bcef
Joseph Miseda <misedajoseph@gmail.com>
2023-04-02 19:23:07 +0300

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *string_toupper - change lowercase letter to uppercase
+ *@str: character to be compared
+ *
+ *Return: characters translated
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0'; i++)
+	{
+		if (str[i] > 'a' && str[i] < 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	str[i] = str[i];
+	}
+return (str);
+}
