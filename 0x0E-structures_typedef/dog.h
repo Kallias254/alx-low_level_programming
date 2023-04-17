9ccf48fd0a3b49289095330613ab8a42e07fbfb8
Joseph Miseda <misedajoseph@gmail.com>
2023-04-17 20:45:13 +0300

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+ /**
+ *struct struct dog - new type
+ *@name: name of dog
+ *@age: dog's age
+ *@owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
